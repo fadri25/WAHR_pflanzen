4a015a0aca7637ec0492d9f361f491aca75aedcf
sarah-inderbitzin <sarah.inderbitzin@outlook.com>
2023-05-15 22:06:48 +0200

    erster versuch

--- main.R
+++ main.R
@@ -1,30 +1,44 @@
+
 #Wasserbedarf denfinieren pro Pflanze
 aloevera_wassererhalt <- rpois(100, lambda = 4)
 baumfreund_wassererhalt <- rpois(1000, lambda = 12)
 
-#Sonnenstunden
-2022 <- c(84,120,	241,	201,	239,	263,	323,	278,	146,	112,	50,	27,	2085)
-2021 <- c(32, 103,	169,	221,	170,	240,	175,	162,	215,	122,	31,	31,	1669)
+#Sonnenstunden von Januar bis Dezember und gesamt in Kloten, ZH
+jahr2022 <- c(84,120,	241,	201,	239,	263,	323,	278,	146,	112,	50,	27,	2085)
+jahr2021 <- c(32, 103,	169,	221,	170,	240,	175,	162,	215,	122,	31,	31,	1669)
+jahr2020 <- c(90, 101,	172,	291,	246,	189,	292,	223,	193,	69,	64,	16,	1945)
 
-N <- 1000
-c(rep(NA,N))
+combined <- cbind(jahr2020, jahr2021, jahr2022)
+combined[1,]
+mean (combined[12,])
+
 N <- 100
 Wohlfühlscore_aloevera <- c(rep(NA,N))
-gesamt <- 
+gesamt <- c(rep(NA,N))
+
 for (i in 1:N){
+  sonnenstunde_matrix <- cbind(jahr2020, jahr2021, jahr2022)
   for (monat in 1:12){
-    sonnenstunden_aloevera <- rnorm(30)
-    jahreszeit <- ifelse(monat <= 6, "sommer", "winter")
-    #jahreszeit <- sample(x = c("sommer", "winter"), size = 1)
+    #pro Monat wird die durchschnittliche Anzahl Sonnenstunden brechnet uafgrund von der matrix
+    mean_sonnenstunden <- mean (sonnenstunde_matrix[monat,])
+    sonnenstunden_aloevera <- rnorm(30, mean = mean_sonnenstunden, sd = 20)
+    mean (sonnenstunden_aloevera)
+    jahreszeit <- ifelse((monat < 6), "sommer", "winter")
+
     aloevera_wassererhalt <- rpois(1, lambda = 4)
     baumfreund_wassererhalt <- rpois(1, lambda = 12)
     if (jahreszeit == "sommer"){
-      ifelse(aloevera_wassererhalt == 4, Wohlfühlscore_aloevera [i]<- 10, Wohlfühlscore_aloevera [i] <- 5)}
+      if(aloevera_wassererhalt == 4) Wohlfühlscore_aloevera [i]<- 10
+      else if (aloevera_wassererhalt == 5 | aloevera_wassererhalt == 3) Wohlfühlscore_aloevera [i] <- 5
+      else Wohlfühlscore_aloevera [i] <- 1}
+    
     else {
-      ifelse(aloevera_wassererhalt == 2, Wohlfühlscore_aloevera [i]<- 9, Wohlfühlscore_aloevera [i] <- 4)
-    }
+      if(aloevera_wassererhalt == 2) Wohlfühlscore_aloevera [i]<- 10
+      else if (aloevera_wassererhalt == 1 | aloevera_wassererhalt == 3) Wohlfühlscore_aloevera [i] <- 5
+      else Wohlfühlscore_aloevera [i] <- 1}
+  durchschnitt <- mean (Wohlfühlscore_aloevera)
   }
-  gesamt 
+  gesamt [i] <- durchschnitt
 }
-Wohlfühlscore_aloevera
-mean (Wohlfühlscore_aloevera)
+gesamt
+mean (gesamt)
